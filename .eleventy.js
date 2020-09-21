bbd94581b0e875c30a0536a83f2e9c0ecda69bb6
Matt Allen <MattAllen17@gmail.com>
2020-09-21 00:01:52 -0400

    html and css minification enabled

--- .eleventy.js
+++ .eleventy.js
@@ -21,11 +21,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // eleventyConfig.addFilter("cssmin", function (code) {
-  //   return new cleanCSS({}).minify(code).styles;
-  // });
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new cleanCSS({}).minify(code).styles;
+  });
 
-  // eleventyConfig.addTransform("htmlmin", htmlMinTransform);
+  eleventyConfig.addTransform("htmlmin", htmlMinTransform);
 
   return {
     dir: {
