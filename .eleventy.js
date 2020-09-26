1fe0eacbddb800bd4244a16deac68737bdabe2c1
Matt Allen <MattAllen17@gmail.com>
2020-09-25 23:20:28 -0400

    manage order of collections

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ const cleanCSS = require("clean-css");
 const htmlMinTransform = require("./_transforms/html-min.js");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+function sortByOrder(values) {
+  let vals = [...values]; // this *seems* to prevent collection mutation...
+  return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+}
+
 module.exports = function (eleventyConfig) {
   // Aliases are in relation to the _includes folder
   eleventyConfig.addLayoutAlias("home", "home.njk");
@@ -21,6 +26,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+  eleventyConfig.addFilter("sortByOrder", sortByOrder);
   eleventyConfig.addFilter("cssmin", function (code) {
     return new cleanCSS({}).minify(code).styles;
   });
